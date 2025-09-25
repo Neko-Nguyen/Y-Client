cd3acef514e864bd666f3b10d99cbed6777a058a
Neko-Nguyen <iywudla@gmail.com>
2025-09-25 22:24:22 +0800

    Change server

--- src/helpers/Constants.js
+++ src/helpers/Constants.js
@@ -1,5 +1,5 @@
 
-export const IS_LOCAL_SERVER = true;
+export const IS_LOCAL_SERVER = false;
 export const LOCAL_API = "http://localhost:3001";
 export const REMOTE_API = "https://y-neko-nguyen-795f0d434f9d.herokuapp.com";
 export const LOCAL_STORAGE = "localAccessToken";
