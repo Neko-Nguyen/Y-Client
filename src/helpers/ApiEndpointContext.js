a412138d2aa254d1295c270f670693ef865db78d
Neko-Nguyen <iywudla@gmail.com>
2025-09-25 19:24:45 +0800

    Fix api endpoint

--- src/helpers/ApiEndpointContext.js
+++ src/helpers/ApiEndpointContext.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 const LOCAL_API = "http://localhost:3001";
-const PROD_API = "https://y-neko-nguyen-795f0d434f9d.herokuapp.com/";
+const PROD_API = "https://y-neko-nguyen-795f0d434f9d.herokuapp.com";
 
 const useProd = false;
 const api = useProd ? PROD_API : LOCAL_API;
